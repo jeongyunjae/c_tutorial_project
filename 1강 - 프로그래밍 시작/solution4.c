d95ee5c3fd99fc09eaf93e13b1a509792de41d6b
ler0n <lyint.kor@gmail.com>
2019-11-06 20:36:02 +0900

    1강 실습문제 정답확인

---
+++ 1강 - 프로그래밍 시작/solution4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d", &a);
+    printf("%d+%d=%d\n", a, 3, a+3);
+    printf("%d-%d=%d\n", a, 3, a-3);
+    printf("%d*%d=%d\n", a, 3, a*3);
+    printf("%d/%d=%d\n", a, 3, a/3);
+    return 0;
+}
+
